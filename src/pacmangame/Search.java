76213ad415760a55471cd2c71904b94af2ba1996
Michael Connor <connorm@wit.edu>
2017-04-10 18:47:25 -0400

    Pushing search again.

--- src/pacmangame/Search.java
+++ src/pacmangame/Search.java
@@ -5,15 +5,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 import java.util.Stack;
-
+/**
+ * 
+ * 
+ */
 public class Search {
 
+	/**
+	 * This method implements a Depth First Search algorithm. Given a start and end position (root and target),
+	 * the algorithm will search to the bottom of each child node branch until the target is found. The updated
+	 * path ArrayList passes the sequence of positions to a move method which calls for a sprite move method.
+	 * 
+	 * @param root starting node
+	 * @param target desired node
+	 */
 	protected static void DFS(Position root, Position target) {
 		root.visited = true;
 		
 		Set<Position> track = new HashSet<Position>();
 		track.add(root);
-		
+			
 		ArrayList<Position> path = new ArrayList<Position>();
 		
 		Stack<Position> s = new Stack<Position>();
@@ -48,6 +59,15 @@ public class Search {
 		sendMoves((Position[])path.toArray());
 	}
 	
+	/**
+	 * This method implements a Breadth First Search algorithm. Given a start and end position (root and target),
+	 * the algorithm will search the current branch node for all children nodes, repeating for all branched children nodes,
+	 * until the target is found. The updated path ArrayList passes the sequence of positions to a move method which 
+	 * calls for a sprite move method.
+	 * 
+	 * @param root starting node
+	 * @param target desired node
+	 */
 	protected static void BFS(Position root, Position target) {
 		root.visited = true;
 		
@@ -89,7 +109,13 @@ public class Search {
 	private void CSF() {
 		
 	}
-	
+	/**
+	 * This method takes a node position and searches up, down, left, and right for unvisited child nodes,
+	 * and passes back a child node that meets those parameters. If no such node exists, returns null.
+	 * 
+	 * @param parent node position
+	 * @return child node position
+	 */
 	private static Position getUnvisitedChildPiece(Position parent) {
 		if (!(BoardState.things.get(Position.getUp(parent)) instanceof Walls) && !(BoardState.things.get(Position.getUp(parent))).whereAmI.visited)
 			return BoardState.things.get(Position.getUp(parent)).whereAmI;
@@ -103,11 +129,23 @@ public class Search {
 			return null;
 	}
 	
+	/**
+	 * This method takes an array of positions and calls the move
+	 * method to carry out search algorithm pathing.
+	 * 
+	 * @param path array of positions
+	 */
 	private static void sendMoves(Position[] path) {
 		for (int i = path.length-1; i > 0 ; i--)
 			Pacman.move(path[i]);
 	}
 	
+	/**
+	 * This method clears all visited nodes of their visited status,
+	 * to reopen them for use in later searches.
+	 * 
+	 * @param track set of all visited nodes
+	 */
 	private static void clearNodes(Set<Position> track) {
 		for (Position p : track)
 			p.visited = false;
